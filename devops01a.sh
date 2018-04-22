964754bddae0317bea264c39bc3154028fe35976
Zafar Chaudry <zafarchaudry@fb.com>
2018-04-22 10:04:02 -0700

    mycommit2

--- devops01a.sh
+++ devops01a.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 vol=$(df -h)
 echo -e "$vol\n"
+echo
 cpu=$(cat /proc/cpuinfo)
 echo -e "$cpu\n"
+echo
 mem=$(cat /proc/meminfo)
 echo -e "$mem\n"
+echo
 ip_mac=$(ipconfig -all | egrep "IPv4|Physi")
 echo -e "$ip_mac\n"
-
+echo
